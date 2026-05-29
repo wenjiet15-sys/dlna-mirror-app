95da4158903a2b5f02398214fa5b8912c7bfd7b0
wenj.tang <wenjiet15@gmail.com>
2026-05-29 17:11:42 +0800

    fix: change 4thline maven repo to http due to failed https connection

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://4thline.org/m2") } // For Cling
+        maven { url = uri("http://4thline.org/m2") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 rootProject.name = "DLNAMirroring"
