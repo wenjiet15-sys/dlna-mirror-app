62cc99017117c0218b1bd917809a8b2b30fa918a
wenj.tang <wenjiet15@gmail.com>
2026-05-29 19:03:09 +0800

    fix: allow insecure protocol for 4thline maven repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("http://4thline.org/m2") }
+        maven {
+            url = uri("http://4thline.org/m2")
+            isAllowInsecureProtocol = true
+        }
         maven { url = uri("https://jitpack.io") }
     }
 }
